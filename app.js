1e61669152b253e45768b3bdb9f1c6942ebb4824
AnushaV1 <anush_chander@yahoo.com>
2020-08-29 22:55:21 -0700

    routes for companies and invoices added

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 /** BizTime express application. */
-
-
 const express = require("express");
-
 const app = express();
 const ExpressError = require("./expressError")
 
 app.use(express.json());
 
+const cRoutes = require("./routes/companies");
+app.use("/companies", cRoutes);
+
+const iRoutes = require("./routes/invoices");
+app.use("/invoices", iRoutes);
 
 /** 404 handler */
 
@@ -17,13 +19,16 @@ app.use(function(req, res, next) {
 });
 
 /** general error handler */
+app.use(function (err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-
-  return res.json({
-    error: err,
-    message: err.message
+  // set the status and alert the user
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status
+    }
   });
 });
 
